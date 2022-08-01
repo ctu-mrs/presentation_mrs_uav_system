e1fafc07458b96fa5910358b2ad3ae6c860d0fcb
Tomas Baca <klaxalk@gmail.com>
2022-08-01 12:24:38 +0200

    updated ci

--- .ci/build.sh
+++ .ci/build.sh
@@ -9,8 +9,9 @@ sudo apt -y update
 sudo apt-get -y install texlive texlive-latex-extra texlive-lang-czechslovak texlive-science texlive-pstricks latexmk texmaker texlive-font-utils texlive-fonts-extra texlive-bibtex-extra biber okular pdf-presenter-console dvipng sketch
 sudo apt -y install poppler-utils imagemagick
 
-pdflatex -interaction=nonstopmode main.tex
-pdftoppm -jpeg main.pdf thumbnail.jpg
-mkdir -p output
-mv main.pdf output/presentation.pdf
-mv montage-1.jpg output/montage-1.jpg
+make
+cd build
+pdftoppm -jpeg main.pdf thumbnail
+mkdir -p ../output
+mv main.pdf ../output/presentation.pdf
+mv thumbnail-1.jpg ../output/thumbnail-1.jpg
